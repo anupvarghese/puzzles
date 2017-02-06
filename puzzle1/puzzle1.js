a2751d846da252a0df62a7e373c24e8e43bba8c3
Anoop Varghese <anupvarghese@gmail.com>
2017-02-06 22:30:10 +1100

    sample test

---
+++ puzzle1/puzzle1.js
@@ -0,0 +1,10 @@
+window.location.href='http://**/';
+var a = function() {
+  document.getElementsByName('valuee')[0].value = Array.prototype.filter.call(document.getElementsByTagName('img'), 
+    function(img) {
+      return img.src === "http://**/filledO.gif";
+    }).length; 
+  document.getElementsByName('redas')[0].value='asdsd';
+  document.getElementsByTagName('form')[0].submit();
+ };
+ window.onload = a();
